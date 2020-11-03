fa011919728265f87fca44a0bb7ef3c5a13d5e62
Ellie Lambert <ebezjian@gmail.com>
2020-11-03 18:08:36 -0500

    add parseinit to inputted age for radio buttons, add id's and values to index.html, add button
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,44 @@
-// User Interface logic
-$(document).ready (function(event) {
-  event.preventDefault();
-  $("form#-").submit(function()) {
 
-const inputtedMovieName = parseInt($("").val());
-const inputtedAge = parseInt($("").val());
-const inputtedTimeOfDay = parseInt($("").val());
+//Business Logic //
 
-let newMovieTicket = new Tickets(inputtedMovieName, inputtedAge, inputtedTimeOfDay);
-let score = add(inputtedMovieName, inputtedAge, inputtedTimeOfDay);
-
-
-
-
-//Business Logic
-}
-  function Tickets (movieName, age, timeOfDay) {
+function Tickets (movieName, age, timeOfDay) {
     this.movieName = movieName;
     this.age = age;
     this.timeOfDay = timeOfDay;
-  }
+}
 
-  Tickets.prototype.printReceipt = function () {
+Tickets.prototype.printReceipt = function () {
     return this.movieName + " " + this.age + " " + this.timeOfDay + this.price;
-  }
+}
 
 
-  function add(inputtedMovieName, inputtedAge, inputtedTimeOfDay) {
+function add(inputtedMovieName, inputtedAge, inputtedTimeOfDay) {
     return inputtedMovieName + inputtedAge + inputtedTimeOfDay;
-  };
+};
+
+if (.score <= 8 && .score > 4) {
+  #show;
+} else if {
+ (.score <=4)
+ #show
+} else if {
+  (.score )
+}
+
+// User Interface Logic //
+
+$(document).ready (function() {
+  $("form#new-ticket").submit(function(event) {
+    event.preventDefault();
+    
+const inputtedMovieName = parseInt($("#movieNames").val());
+const inputtedAge = parseInt($("input[name='ageInput']:checked").val());
+const inputtedTimeOfDay = parseInt($("#times").val());
 
 
 
-    // if (this.age < 18) {
-    //   return 
-    // } 
-    // if (this.age >= 18 && this.age < 65) {
-    //   return 
-    // }
-    // if (this.age >= 65) {
+let newMovieTicket = new Tickets(inputtedMovieName, inputtedAge, inputtedTimeOfDay);
+let score = add(inputtedMovieName, inputtedAge, inputtedTimeOfDay);
+  }); 
+});
 
-    // }
-    // if (this.age >= 100) {
-
-    // }
