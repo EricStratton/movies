e216bbf1ceb7aa72eea2f7312a4d0b46df490ac7
Ellie Lambert <ebezjian@gmail.com>
2020-11-03 14:41:10 -0500

    add objects and prototypes to scripts.js
    Co-authored-by: Eric Stratton <strattonericj@gmail.com>
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+| Behavior                                                        | Input     | Output    |
+| --------------------------------------------------------------- | --------- | --------- |
+| name of movie |  | Expect().toEqual(); |
+| age| (click)Australia opens to >blah blah|  |
+| time of day | .addsLocation | "Paris" |
---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// User Interface logic
+
+
+const inputtedMovieName = parseInt($("").val());
+const inputtedAge = parseInt($("").val());
+const inputtedTimeOfDay = parseInt($("").val());
+
+let newMovieTicket = new Tickets(inputtedMovieName, inputtedAge, inputtedTimeOfDay);
+let score = add(inputtedMovieName, inputtedAge, inputtedTimeOfDay);
+
+
+
+
+//Business Logic
+function Tickets (movieName, age, timeOfDay) {
+  this.movieName = movieName;
+  this.age = age;
+  this.timeOfDay = timeOfDay;
+}
+
+Tickets.prototype.printReceipt = function () {
+  return this.movieName + " " + this.age + " " + this.timeOfDay + this.price;
+}
+
+
+function add(inputtedMovieName, inputtedAge, inputtedTimeOfDay) {
+  return inputtedMovieName + inputtedAge + inputtedTimeOfDay;
+};
+
+
+
+    // if (this.age < 18) {
+    //   return 
+    // } 
+    // if (this.age >= 18 && this.age < 65) {
+    //   return 
+    // }
+    // if (this.age >= 65) {
+
+    // }
+    // if (this.age >= 100) {
+
+    // }
